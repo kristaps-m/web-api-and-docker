bb92912aca893f799befa4fa366b00c65384ebd2
Kristaps-M <kristapsmitins@gmail.com>
2023-04-19 21:02:40 +0300

    Add project files.

---
+++ web-api-and-docker/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_api_and_docker.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+
+    }
+}
